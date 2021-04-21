c857a2062f5fb24e11dc443578da505a7e538d8c
ic.liang <ic.liang@loreal.com>
2021-04-21 17:39:42 +0800

    完成date逻辑和在数据库新增数据

--- app/config/dbConf.js
+++ app/config/dbConf.js
@@ -6,7 +6,7 @@
 // const connectionStr =
 //   'mongodb+srv://iCbaby:456123Ab13@cluster0.bppbk.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
-const connectionStr = 'mongodb://10.167.64.40:27017/massage_chair_prod' // 本地测试数据库
+const connectionStr = 'mongodb://10.167.64.40:27017/massage_chair_dev' // 本地测试数据库
 
 // const connectionStr = 'mongodb://10.167.64.40:27017/massage_chair_prod' // 本地生产数据库
 
